15b0310bc7080e80ac5f90baa3954754ff70fb05
carloszbent <bentsoft365@gmail.com>
2023-05-31 22:17:05 -0400

    added converASCIICharsToText function

--- index.js
+++ index.js
@@ -13,4 +13,31 @@ function convertMarkdownToPlainText(markdownText, markedOptions = defaultOptions
     return marked(markdownText, { renderer })
   }
 
-export { convertMarkdownToPlainText }
+function __convertASCIINamesToText(str) {
+  // convert HTML Names (eg. "&amp;" => "&")
+  /* every HTMLName replacement needs to be a 
+  separate statement, because string.replace() 
+  can only replace one value at a time.*/
+  let replaceAmp = str.replaceAll("&quot;", '"');
+  let replaceQuot = replaceAmp.replaceAll("&amp;", "&");
+  let convertedHTMLNames = replaceQuot;
+  return convertedHTMLNames
+}
+
+function __convertASCIINumbersToText(str) {
+  // convert HTML Numbers (eg. "&#36;" => $)
+  return str.replace(/&#(\d+);/g, (match, code) => {
+    let convertedHTMLNumbers = String.fromCharCode(Number(code));
+    return convertedHTMLNumbers
+  });
+}
+
+function convertASCIICharsToText(str) {
+  let conversion1 = __convertASCIINamesToText(str);
+  let conversion2 = __convertASCIINumbersToText(conversion1);
+  let processedString = conversion2;
+  return processedString
+}
+
+
+export { convertMarkdownToPlainText, convertASCIICharsToText }
